a806fc38555f4f7d7ab968c350626277d8e1116c
Binny V A <binnyva@binlap.(none)>
2010-10-30 00:33:00 +0530

    Added some text to make things clearer for the user

--- templates/index.php
+++ templates/index.php
@@ -1,5 +1,7 @@
 <h2>Most Popular DPs</h2>
 
+<p>Click on any of the images shown below to set it as your DP. If you don't like the images listed here, use the search option to get more images.</p>
+
 <?php foreach($latest_dps as $dp) { ?>
 <a class="image-preview" href="set_dp.php?file=<?php echo $dp['file'] ?>"><img src="dp/thumbs/<?php echo $dp['file'] ?>" longdesc="dp/full/<?php echo $dp['file_name'] ?>" /></a>
 
--- templates/search.php
+++ templates/search.php
@@ -1,6 +1,8 @@
 <h2>Results for <em><?php echo $PARAM['keyword']; ?></em></h2>
 
+<p>Click on any of the images shown below to set it as your DP.</p>
+
 <?php foreach($images as $img) { ?>
-<a class="image-preview" href="set_dp.php?url=<?php echo urlencode($img) ?>"><img src="system/thumb.php?url=<?php echo urlencode($img) ?>" /></a>
+<a class="image-preview" href="set_dp.php?url=<?php echo urlencode($img) ?>"><img src="system/thumb.php?url=<?php echo urlencode($img) ?>" longdesc="<?php echo $img ?>" /></a>
 
 <?php } ?>
